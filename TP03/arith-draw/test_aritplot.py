904259345812af48d0c564ee7c3a89e2f7fc5d01
Valentin Lorentz <valentin.lorentz+git@ens-lyon.org>
2017-09-24 10:56:42 +0200

    automated commit TP3

--- TP03/arith-draw/test_aritplot.py
+++ TP03/arith-draw/test_aritplot.py
@@ -9,11 +9,11 @@ Usage:
 import unittest
 import fnmatch
 import os
-import arit
 import re
 import io
-
 from contextlib import redirect_stdout
+
+import arit
 
 HERE = os.path.dirname(os.path.realpath(__file__))
 
@@ -24,12 +24,14 @@ class TestAritEval(unittest.TestCase):
 
     def all_files_in_dir(self, dir):
         for file in os.listdir(dir):
+            path = os.path.join(dir, file)
             if fnmatch.fnmatch(file, '*.txt'):
-                self.one_file(os.path.join(dir, file))
+                with self.subTest(msg=path):
+                    self.one_file(path)
 
     def extract_expect(self, file):
-        state = 'outside'
-        result = ''
+        inside_expected = False
+        expected_lines = []
         with open(file, encoding="utf-8") as f:
             for line in f.readlines():
                 # Ignore non-comments
@@ -40,19 +42,21 @@ class TestAritEval(unittest.TestCase):
                 line = re.sub('\s*$', '', line)
 
                 if line == 'END EXPECTED':
-                    state = 'outside'
+                    inside_expected = False
 
-                if state == 'inside_expected':
-                    result += line + '\n'
+                if inside_expected:
+                    expected_lines.append(line)
 
                 if line == 'EXPECTED':
-                    state = 'inside_expected'
-        return result
+                    inside_expected = True
+
+        expected_lines.append('')
+        return '\n'.join(expected_lines)
 
     def remove(self, file):
         try:
             os.remove(file)
-        except:
+        except OSError:
             pass
 
     def execute(self, file):
@@ -66,15 +70,10 @@ class TestAritEval(unittest.TestCase):
         return f.getvalue()
 
     def one_file(self, file):
-        print("analysing "+file)
         expect = self.extract_expect(file)
-        print(expect)
         actual = self.execute(file)
-        print(actual)
-        print("Result for", file)
         if expect:
             self.assertEqual(actual, expect)
-        print(file, ": OK")
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main() # runs TestAritEval.test_ex()
